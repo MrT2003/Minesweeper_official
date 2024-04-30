215bd7b2a41636a7bb1a49c099e356178820408d
MrT2008 <haitrieu6212@gmail.com>
2024-04-30 11:25:20 +0700

    update fourth time

--- src/controller/World.java
+++ src/controller/World.java
@@ -18,35 +18,60 @@ public class World {
         arrayButton = new ButtonPlayer[w][h];
         arrayBoom = new int[w][h];
         random = new Random();
+
         createArrayBoom(boom, w, h);
+        showNumber();
+        System.out.println(boom);
         for(int i = 0; i < arrayBoom.length; i++) {
-            for(int j = 0; j < arrayBoom[i].length; j++) {
-                System.out.print(arrayBoom[i][j] + "");
+            for(int j = 0; j < arrayBoom.length; j++)  {
+                System.out.print(arrayBoom[i][j] + " ");
             }
             System.out.println();
         }
 
     }
 
+    public void showNumber() {
+        for(int i = 0; i < arrayBoom.length; i++) {
+            for(int j = 0; j < arrayBoom.length; j++) {
+                if(arrayBoom[i][j] == 0) {
+                    int count = 0;
+                    for(int l = i - 1; l <= i + 1; l++) {
+                        for(int k = j - 1; k <= j + 1; k++) {
+                            if(l >= 0 && l <= arrayBoom.length - 1 && k >= 0 && k <= arrayBoom.length - 1) {
+                                if(arrayBoom[l][k] == -1) {
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                    arrayBoom[i][j] = count;
+                }
+            }
+        }
+    }
+
+
     public void createArrayBoom(int boom, int w, int h) {
         int location_x = random.nextInt(w);
         int location_y = random.nextInt(h);
 
-        arrayBoom[location_x][location_y] = 10;             /*Size arrayBoom = 10*/
+        arrayBoom[location_x][location_y] = -1;             /*Size arrayBoom = 1  */
         int count = 1;
         while(count != boom) {
             location_x = random.nextInt(w);
             location_y = random.nextInt(h);
-            if(arrayBoom[location_x][location_y] != 10) {
-//                arrayBoom[location_x][location_y] = 10;
+            if(arrayBoom[location_x][location_y] != -1) {
+                arrayBoom[location_x][location_y] = -1;
                 count = 0;
                 for(int i = 0; i < arrayBoom.length; i++) {
                     for(int j = 0; j < arrayBoom.length; j++) {
-                        count++;
+                        if(arrayBoom[i][j] == -1) {
+                            count++;
+                        }
                     }
                 }
             }
-
         }
     }
 
